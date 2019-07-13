af0bf1bb5b7d50b9b59f107bf5952448d8b465fb
josh <joshmeranda@gmail.com>
2019-07-12 23:13:44 -0400

    Fix Repo class properties

--- cli/src/main/kotlin/com/jmeranda/glazy/cli/Main.kt
+++ cli/src/main/kotlin/com/jmeranda/glazy/cli/Main.kt
@@ -1,20 +1,25 @@
 package com.jmeranda.glazy.cli
 
+import com.jmeranda.glazy.lib.Issue
 import kotlin.system.exitProcess
 
 import com.jmeranda.glazy.lib.Repo
-import com.jmeranda.glazy.lib.service.RepoService
+import com.jmeranda.glazy.lib.exception.NoSuchRepo
+import com.jmeranda.glazy.lib.service.*
 
 fun main(args: Array<String>) {
-    println("=== READING ===")
-    val (user: String?, name: String?) = getRepoName()
-
-    println("user: $user\nrepo: $name")
+    var (user: String?, name: String?) = getRepoName()
 
     if (user == null || name == null) { exitProcess(1) }
 
-    val service = RepoService()
-    val repo: Repo? = service.getRepo(name, user)
+    name = "bash-full"
 
-    println("=== DONE ===")
+    val service = RepoService()
+    val repo: Repo = service.getRepo(name, user) ?: throw NoSuchRepo(name)
+    val issueService = IssueService(repo)
+    val issues = issueService.getAllIssues()
+
+    for (i: Issue in issues) {
+        println("issue: ${i.title}")
+    }
 }
