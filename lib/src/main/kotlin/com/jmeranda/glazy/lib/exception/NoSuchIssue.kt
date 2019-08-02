1865afa6901442cf205f1fea3d73e92462f1d889
josh <joshmeranda@gmail.com>
2019-08-02 12:29:31 -0400

    Migrate to Picocli; implement issue listing

--- lib/src/main/kotlin/com/jmeranda/glazy/lib/exception/NoSuchIssue.kt
+++ lib/src/main/kotlin/com/jmeranda/glazy/lib/exception/NoSuchIssue.kt
@@ -1,5 +1,8 @@
 package com.jmeranda.glazy.lib.exception
 
+/**
+ * Exception for non-existent Issue.
+ */
 class NoSuchIssue(
         private val number: Int,
         override val message: String = "No such issue '$number'"
--- lib/src/main/kotlin/com/jmeranda/glazy/lib/exception/NoSuchRepo.kt
+++ lib/src/main/kotlin/com/jmeranda/glazy/lib/exception/NoSuchRepo.kt
@@ -1,5 +1,8 @@
 package com.jmeranda.glazy.lib.exception
 
+/**
+ * Exception for non-existent repository.
+ */
 class NoSuchRepo(
         private val repo: String,
         override val message: String = "No such repository '$repo'"
