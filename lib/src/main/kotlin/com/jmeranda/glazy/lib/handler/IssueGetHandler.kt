8518d5e58fd571a4cb135f3a9bb5347e9636a789
josh <joshmeranda@gmail.com>
2019-08-12 22:23:07 -0400

    Handler reader and writer

--- lib/src/main/kotlin/com/jmeranda/glazy/lib/handler/IssueGetHandler.kt
+++ lib/src/main/kotlin/com/jmeranda/glazy/lib/handler/IssueGetHandler.kt
@@ -9,6 +9,7 @@ import com.jmeranda.glazy.lib.request.IssueGetRequest
 
 /**
  * Handle GET request for a specific.
+ *
  * @property issueRequest The request object used by the handler.
  * @property token The personal access token of the user.
  */
@@ -24,7 +25,7 @@ class IssueGetHandler(
         var issue: Issue?
 
         try {
-            issue = Handler.mapper.readValue(issueAsJson)
+            issue = Handler.reader().readValue(issueAsJson)
         } catch (e: Exception) {
             issue = null
         }
