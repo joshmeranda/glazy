2903034673478bed98a12cd8ae475005b1cb1496
josh <joshmeranda@gmail.com>
2019-09-05 19:10:18 -0400

    Fix linting errors

--- cli/src/main/kotlin/com/jmeranda/glazy/cli/Main.kt
+++ cli/src/main/kotlin/com/jmeranda/glazy/cli/Main.kt
@@ -11,8 +11,7 @@ import com.jmeranda.glazy.cli.commands.*
 @Command(name = "glazy",
         description = ["A command line interface to the github api."],
         mixinStandardHelpOptions = true)
-class Glazy() {
-}
+class Glazy
 
 fun main(args: Array<String>) {
     CommandLine(Glazy())
